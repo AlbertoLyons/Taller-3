a0b17402348ca8b399956384c460a964be506213
AlbertoLyons <alberto.lyons@alumnos.ucn.cl>
2023-05-02 14:51:54 -0400

    Base

---
+++ src/Sistema.java
@@ -0,0 +1,7 @@
+public interface Sistema {
+    public void agregarInstrumento();
+    public void venderInstrumento();
+    public void consultarInventario();
+
+
+}
---
+++ src/SistemaImpl.java
@@ -0,0 +1,17 @@
+public class SistemaImpl implements Sistema{
+
+    @Override
+    public void agregarInstrumento() {
+
+    }
+
+    @Override
+    public void venderInstrumento() {
+
+    }
+
+    @Override
+    public void consultarInventario() {
+
+    }
+}
